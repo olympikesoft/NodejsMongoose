777cfd07e91a38769b96369d9df0327459d1b83a
bechir <nemlaghi.bechir@gmail.com>
2017-10-30 01:02:29 +0100

    fixes #1 db connection was not established

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -1,38 +1,25 @@
 var mongoose = require('mongoose');
-var express = require("express");
-var app = express();
-var router = express.Router();
-
-
+var router = require("express").Router();
 
 var User = require('../models/users.js');
 
-
 router.post('/insert', insertUsert);
 
-    function insertUsert(req, res, next){
-        const newUser = new User({
-            name: req.body.name,
-            username: req.body.username, 
-            password: req.body.password, 
-            admin: false
+function insertUsert(req, res) {
 
-         })
+  const newUser = new User({
+    name: req.body.name,
+    username: req.body.username, 
+    password: req.body.password, 
+    admin: false
+  })
 
-newUser.save().then(function(user) {
+  newUser.save(function(err) {
+    if(err)
+      return res.status(400).json(err)
+    return res.status(200).json(newUser)
 
-    if(err) res.status(400).json({"message": err})
-    res.status(200).json(user)
-
-})
-        }
-  /*
-
-  router.post('/user/insert', function insertUsert(req, res){
-    res.status(200).send("hi");
-  });
-
-  */
-  
+  })
+}
 
 module.exports = router;
